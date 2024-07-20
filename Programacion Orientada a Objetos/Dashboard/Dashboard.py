a625e4532a1d4604d38fad440f8c4643b6429962
Alexis uea <159032726+Alexisqwer@users.noreply.github.com>
2024-07-20 16:34:28 +0200

    semana 8

--- Programacion Orientada a Objetos/Dashboard/Dashboard.py
+++ Programacion Orientada a Objetos/Dashboard/Dashboard.py
@@ -3,25 +3,25 @@ import subprocess
 
 # Constantes
 MENU_OPTIONS = {
-    '1': 'TÃ©cnicas de ProgramaciÃ³n',
+    '1': 'Técnicas de Programación',
     '2': 'Programacion tradicional frente a POO',
     '3': 'EjemplosMundoReal_POO',
     '4': 'Tipos de datos, Identificadores',
     '5': 'Clases, Objetos, Herencia, Encapsulamiento y Polimorfismo',
     '6': 'Constructores y Destructores',
-    '7': 'TÃ©cnicas de ProgramaciÃ³n',
+    '7': 'Dashboard',
 }
 
 def mostrar_codigo(ruta_script):
     ruta_script_absoluta = os.path.abspath(ruta_script)
     try:
         with open(ruta_script_absoluta, 'r') as archivo:
-            print(f"\n--- CÃ³digo de {ruta_script} ---\n")
+            print(f"\n--- Código de {ruta_script} ---\n")
             print(archivo.read())
     except FileNotFoundError:
-        print("El archivo no se encontrÃ³.")
+        print("El archivo no se encontró.")
     except Exception as e:
-        print(f"OcurriÃ³ un error al abrir el archivo: {e}")
+        print(f"Ocurrió un error al abrir el archivo: {e}")
 
 def mostrar_contenido_carpeta(ruta_carpeta):
     print(f"\nContenido de la carpeta {ruta_carpeta}:")
@@ -43,14 +43,14 @@ def mostrar_menu():
         for key, value in MENU_OPTIONS.items():
             print(f"{key} - {value}")
         print("0 - salir")
-        eleccion = input("Escribe una opciÃ³n: o '0' para salir ")
+        eleccion = input("Escribe una opción: o '0' para salir ")
         if eleccion == '0':
             break
         elif eleccion in MENU_OPTIONS:
             ruta_carpeta = os.path.join(ruta_base, MENU_OPTIONS[eleccion])
             if os.path.isdir(ruta_carpeta):
                 mostrar_contenido_carpeta(ruta_carpeta)
-                respuesta = input("Â¿Desea ejecutar los archivos Python en la carpeta? (s/n): ")
+                respuesta = input("¿Desea ejecutar los archivos Python en la carpeta? (s/n): ")
                 if respuesta.lower() == 's':
                     # Busca los archivos Python en la carpeta seleccionada
                     archivos_python = [f for f in os.listdir(ruta_carpeta) if f.endswith('.py')]
@@ -61,11 +61,11 @@ def mostrar_menu():
                             ejecutar_archivo_python(ruta_archivo)
                             print(f"Archivo {archivo_python} ejecutado correctamente.")
                     else:
-                        print("No se encontrÃ³ ningÃºn archivo Python en la carpeta.")
+                        print("No se encontró ningún archivo Python en la carpeta.")
             else:
                 print(f"La carpeta {MENU_OPTIONS[eleccion]} no existe.")
         else:
-            print("OpciÃ³n no vÃ¡lida. Por favor, intente de nuevo")
+            print("Opción no válida. Por favor, intente de nuevo")
 
 if __name__ == '__main__':
     mostrar_menu()
