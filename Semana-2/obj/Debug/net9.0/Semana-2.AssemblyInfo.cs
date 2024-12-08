4d0faac1217bc48b93514aa5ee4c925557604b99
Alexis Merino <ae.merinom@uea.edu.ec>
2024-12-08 21:09:00 +0100

    -

--- Semana-2/obj/Debug/net9.0/Semana-2.AssemblyInfo.cs
+++ Semana-2/obj/Debug/net9.0/Semana-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d8cae81dbf6609c7dbf7df0c55ca74c563a1a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+322019bbf579b2257c0508214ff44564a092a699")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
